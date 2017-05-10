358ee0cf5f601fbcda166fe635939a45bea321b0
DouglasCristhian (NoteBook DELL) <dc@weblayer.com.br>
2017-05-10 15:00:32 -0300

    --

--- weblayer.embarcador.android/Activities/CenarioEntrega/Activity_CenarioEntrega.cs
+++ weblayer.embarcador.android/Activities/CenarioEntrega/Activity_CenarioEntrega.cs
@@ -123,7 +123,6 @@ namespace weblayer.embarcador.android.Activities
             var categoryAxis1 = new CategoryAxis()
             {
                 Title = "Período da Entrega",
-                //AbsoluteMaximum = intMax.nr_dias,
                 MaximumRange = 15
             };
 
@@ -180,30 +179,27 @@ namespace weblayer.embarcador.android.Activities
             CenarioEntregaManager manager = new CenarioEntregaManager();
             List<CenarioEntrega> list = manager.GetCenario2(ano, mes);
 
-            List<CenarioEntrega> SortedList = list.OrderBy(o => -o.nr_dias).ToList();
+            List<CenarioEntrega> SortedList = list.OrderByDescending(o => -o.nr_dias).ToList();
 
             var barSeries = new BarSeries { Title = "Entrega até o Prazo", StrokeColor = OxyColors.Black, StrokeThickness = 1 };
             var categoryAxis = new CategoryAxis { Position = AxisPosition.Left };
 
             for (int i = 0; i < SortedList.Count; i++)
             {
-                if (SortedList[i].nr_dias >= 0)
-                {
-                    categoryAxis.Labels.Add((SortedList[i].nr_dias * 1).ToString() + " dias");
-                    barSeries.Items.Add(new BarItem(SortedList[i].nr_notas) { Color = OxyColors.Green });
-                }
-            }
-
-            int valorMaximoX = SortedList.Max(r => r.nr_notas) + 10;
-
-            for (int i = 0; i < SortedList.Count; i++)
-            {
-                if (SortedList[i].nr_dias < 0)
+                if (SortedList[i].nr_dias <= 0)
                 {
                     categoryAxis.Labels.Add((SortedList[i].nr_dias * -1).ToString() + " dias");
+                    barSeries.Items.Add(new BarItem(SortedList[i].nr_notas) { Color = OxyColors.Green });
+                }
+                if (SortedList[i].nr_dias > 0)
+                {
+                    categoryAxis.Labels.Add((SortedList[i].nr_dias * 1).ToString() + " dias");
                     barSeries.Items.Add(new BarItem(SortedList[i].nr_notas) { Color = OxyColors.Red });
                 }
             }
+
+
+            int valorMaximoX = SortedList.Max(r => r.nr_notas) + 10;
 
             categoryAxis.AbsoluteMinimum = -1;
             categoryAxis.AbsoluteMaximum = SortedList.Count;
